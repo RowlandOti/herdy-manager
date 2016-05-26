4e3b925e294b825aaa205eb83bde040341cd5c9d
RowlandOti <rowlandotienoo@gmail.com>
2016-05-26 09:14:18 +0300

    fix: rectify package path

--- app/src/main/java/co/herdy/manager/presentation/internal/di/components/ApplicationComponent.java
+++ app/src/main/java/co/herdy/manager/presentation/internal/di/components/ApplicationComponent.java
@@ -1,19 +1,4 @@
-/**
- * Copyright (C) 2015 Fernando Cejas Open Source Project
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in copliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package co.herdy.manager.presentation.internal.di.coponents;
+package co.herdy.manager.presentation.internal.di.components;
 
 import android.content.Context;
 
@@ -28,9 +13,9 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 /**
- * A coponent whose lifetime is the life of the application.
+ * A component whose lifetime is the life of the application.
  */
-@Singleton // Constraints this coponent to one-per-application or unscoped bindings.
+@Singleton // Constraints this component to one-per-application or unscoped bindings.
 @Component(modules = ApplicationModule.class)
 public interface ApplicationComponent {
     void inject(ABaseActivity baseActivity);
